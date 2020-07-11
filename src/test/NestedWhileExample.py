3b5a0a8df912f0b8a06e731f61867b469c6e832c
RamuTalari <ramu.t9@gmail.com>
2020-07-11 20:42:06 +0100

    python practice

---
+++ src/test/NestedWhileExample.py
@@ -0,0 +1,10 @@
+i = 1
+while(i<=5):
+    print("London ",end="")
+    j=1
+    while(j<=4):
+        print("UK ",end="")
+        j=j+1
+
+    i=i+1
+    print()
---
+++ src/test/ifelifExample.py
@@ -0,0 +1,14 @@
+x = int(input("Enter number:"))
+
+if x == 1:
+    print("One")
+elif x==2:
+    print("Two")
+elif x==3:
+    print("Three")
+elif x==4:
+    print("Four")
+elif x==5:
+    print("Five")
+else:
+    print("wrong input")
